ab1082b6d299a151bd80b3463d2376fc611b83b5
Zuko <zuko@doma.cooking>
2026-03-24 05:19:31 +0000

    KIT-67: run pip install as node user to avoid root warning

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,7 +47,7 @@ if [ -n "$PLUGINS_GIT_URL" ]; then
 
   if [ -f "$PLUGINS_DIR/requirements.txt" ]; then
     echo "Installing Python dependencies from plugins..."
-    pip3 install --break-system-packages -q -r "$PLUGINS_DIR/requirements.txt"
+    gosu node pip3 install --user -q -r "$PLUGINS_DIR/requirements.txt"
   fi
 
   chown -R node:node "$PLUGINS_DIR"
